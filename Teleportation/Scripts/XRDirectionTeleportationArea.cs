68b0bc22cd517e31714bdf612db431b35571bfee
Jan Löhr <janloehr@live.com>
2020-03-28 21:52:21 +0100

    Added Teleportation Oculus Style

---
+++ Teleportation/Scripts/XRDirectionTeleportationArea.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRDirectionTeleportationArea : BaseTeleportationInteractable
+{
+    private bool _updateReticle;
+
+    Dictionary<XRBaseInteractor, GameObject> m_ReticleCache = new Dictionary<XRBaseInteractor, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var item in m_ReticleCache)
+        {
+            XRController controller = item.Key.GetComponent<XRController>();
+
+            if (controller.inputDevice.TryGetFeatureValue(new InputFeatureUsage<Vector2>("Primary2DAxis"), out Vector2 value))
+            {
+                if (value.magnitude > (controller as XRTeleportationController).TriggerTeleportThreshold)
+                {
+                    Vector3 camForward = Camera.main.transform.forward;
+                    camForward.y = 0;
+                    camForward = Quaternion.LookRotation(new Vector3(value.x, 0, value.y)) * camForward;
+                    item.Value.transform.GetChild(0).rotation = Quaternion.LookRotation(camForward);
+                }
+                else
+                {
+                    TeleportRequest tr = new TeleportRequest();
+                    tr.matchOrientation = MatchOrientation.Camera;
+                    tr.requestTime = Time.time;
+                    if (GenerateTeleportRequest(item.Key, new RaycastHit(), ref tr))
+                    {
+                        m_TeleportationProvider.QueueTeleportRequest(tr);
+                    }
+
+                    onSelectEnter.Invoke(item.Key);
+                }
+            }
+        }
+    }
+
+    protected override bool GenerateTeleportRequest(XRBaseInteractor interactor, RaycastHit raycastHit, ref TeleportRequest teleportRequest)
+    {
+        Transform lookTrans = m_ReticleCache[interactor].transform.GetChild(0);
+
+        teleportRequest.destinationPosition = lookTrans.position;
+        teleportRequest.destinationUpVector = lookTrans.up; // use the area transform for data.
+        teleportRequest.destinationForwardVector = lookTrans.forward;
+        teleportRequest.destinationRotation = lookTrans.rotation;
+
+        return true;
+    }
+
+    public override void AttachCustomReticle(XRBaseInteractor interactor)
+    {
+        if (interactor == null)
+            return;
+
+        // try and find any attached reticle and swap it
+        IXRCustomReticleProvider ilv = interactor.transform.GetComponent<IXRCustomReticleProvider>();
+        if (ilv != null)
+        {
+
+            GameObject prevReticle;
+            if (m_ReticleCache.TryGetValue(interactor, out prevReticle))
+            {
+                Destroy(prevReticle);
+                m_ReticleCache.Remove(interactor);
+            }
+            if (customReticle != null)
+            {
+                var rInstance = Instantiate(customReticle);
+                m_ReticleCache.Add(interactor, rInstance);
+                ilv.AttachCustomReticle(rInstance);
+            }
+        }
+    }
+
+    public override void RemoveCustomReticle(XRBaseInteractor interactor)
+    {
+        if (interactor == null)
+            return;
+
+        // try and find any attached reticle and swap it            
+        IXRCustomReticleProvider ilv = interactor.transform.GetComponent<IXRCustomReticleProvider>();
+        if (ilv != null)
+        {
+            GameObject reticle;
+            bool setCustomReticle = false;
+            if (m_ReticleCache.TryGetValue(interactor, out reticle))
+            {
+                Destroy(reticle);
+                m_ReticleCache.Remove(interactor);
+                setCustomReticle = true;
+            }
+            if (setCustomReticle)
+            {
+                ilv.RemoveCustomReticle();
+            }
+        }
+    }
+}
---
+++ Teleportation/Scripts/XRTeleportationController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRTeleportationController : XRController
+{
+    [Header("Teleporting")]
+
+    [SerializeField]
+    [Range(0, 1)]
+    [Tooltip("The magnitude of the AxisVector where the teleport will be triggered when lower than that")]
+    private float _triggerTeleportThreshold = 0.3f;
+    public float TriggerTeleportThreshold => _triggerTeleportThreshold;
+
+    private bool _performSetup = true;
+
+    private GameObject _modelGameObject = null;
+
+    void Update()
+    {
+        if (_performSetup)
+        {
+            PerformSetup();
+            _performSetup = false;
+        }
+
+        if (enableInputTracking &&
+            (updateTrackingType == UpdateType.Update ||
+            updateTrackingType == UpdateType.UpdateAndBeforeRender))
+        {
+            UpdateTrackingInput();
+
+        }
+
+        if (enableInputActions)
+        {
+            UpdateInput();
+        }
+    }
+
+    void UpdateInput()
+    {
+        // clear state for selection, activation and press state dependent on this frame
+        m_SelectInteractionState.activatedThisFrame = m_SelectInteractionState.deActivatedThisFrame = false;
+
+        HandleInteractionAction(InputHelpers.Button.Trigger, ref m_SelectInteractionState);
+    }
+
+    void HandleInteractionAction(InputHelpers.Button button, ref InteractionState interactionState)
+    {
+        if (inputDevice.TryGetFeatureValue(new InputFeatureUsage<Vector2>("Primary2DAxis"), out Vector2 value))
+        {
+            if (value.magnitude > _triggerTeleportThreshold)
+            {
+                m_SelectInteractionState.active = true;
+            }
+            else if (m_SelectInteractionState.active)
+            {
+                m_SelectInteractionState.active = false;
+                m_SelectInteractionState.deActivatedThisFrame = true;
+            }
+        }
+    }
+
+    void PerformSetup()
+    {
+        SetupModel();
+    }
+
+    void SetupModel()
+    {
+        if (_modelGameObject)
+            Destroy(_modelGameObject);
+        var model = modelPrefab;
+        if (model != null)
+        {
+            _modelGameObject = Instantiate(model).gameObject;
+            _modelGameObject.transform.parent = modelTransform;
+            _modelGameObject.transform.localPosition = new Vector3(0f, 0f, 0f);
+            _modelGameObject.transform.localRotation = Quaternion.identity;
+            _modelGameObject.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            _modelGameObject.transform.gameObject.SetActive(true);
+        }
+    }
+}
