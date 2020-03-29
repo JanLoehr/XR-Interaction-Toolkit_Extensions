e1bc7cfdc0baa2da805fecaa1ccd2b6453c2f166
Jan Löhr <janloehr@live.com>
2020-03-29 12:00:50 +0200

    added grabbing at contact point

---
+++ Runtime/Interaction/XRGrabPointInteractable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace XR.Interaction.Toolkit.Extensions
+{
+    public class XRGrabPointInteractable : XRGrabInteractable
+    {
+        [SerializeField]
+        private bool _attachAtContactPoint = true;
+
+        private Transform _previousAttachTransform;
+
+        protected override void OnSelectEnter(XRBaseInteractor interactor)
+        {
+            if (_attachAtContactPoint)
+            {
+                _previousAttachTransform = attachTransform;
+
+                Transform tempAttachTrans = new GameObject("TempAttachTransform").transform;
+                tempAttachTrans.SetParent(transform);
+                tempAttachTrans.SetPositionAndRotation(interactor.transform.position, interactor.transform.rotation);
+
+                attachTransform = tempAttachTrans;
+            }
+
+            base.OnSelectEnter(interactor);
+        }
+
+        protected override void OnSelectExit(XRBaseInteractor interactor)
+        {
+            base.OnSelectExit(interactor);
+
+            if (_attachAtContactPoint)
+            {
+                Destroy(attachTransform.gameObject);
+
+                attachTransform = _previousAttachTransform;
+            }
+        }
+    }
+}
